e801264e80ad6e9868bd620d639eccc004a59763
unknown <erika.lyde@gmail.com>
2020-05-25 17:20:49 -0300

    Verifique que la API no me traiga un array vacio en el campo genero

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -381,3 +381,25 @@ test('Finalizar un libro disponible por api', async () => {
     // Verificamos que falle la request
     expect(putRequest.status).toBe(400);
 });
+test('Al btener libros por api que el campo genero no este vacio', async () => {
+    const bookData = {
+        title: 'El Aleph',
+        synopsis:
+            'Este volumen reúne dieciocho relatos de Jorge Luis Borges, entre ellos quizá los más elogiados y repetidamente citados. Tanto «El inmortal» como «Los teólogos», «Deutsches Requiem» y «La espera» muestran las posibilidades expresivas de la «estética de la inteligencia» borgiana, inimitable fusión de mentalidad matemática, profundidad metafísica y captación poética del mundo.',
+        year: 1949,
+        publisher: 'Editorial Losada',
+        isbn: '9788499089515',
+        genres: ['Cuentos', 'Fantástico'],
+        authors: ['Jorge Luis Borges'],
+        cover: '/assets/el-aleph.jpg',
+    };
+
+    // Creamos el libro
+    await BookModels.create(bookData);
+
+    const URL = `${baseURL}/books`;
+    const req = await fetch(URL);
+    const books = await req.json();
+
+    expect(books[0].genres.length).not.toBe(0);
+});
